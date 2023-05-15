e0d29285779c84fac6e7f59fe80744c35a025e54
Henning <h.patrzich@campus.tu-berlin.de>
2023-05-16 00:05:29 +0200

    Added for-loop to change freespeed for all links in Berlin except motorways.

--- src/main/java/org/matsim/prepare/ImplementCityWideSpeedLimit.java
+++ src/main/java/org/matsim/prepare/ImplementCityWideSpeedLimit.java
@@ -9,11 +9,12 @@ import org.matsim.core.network.NetworkUtils;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class ImplementCityWideSpeedLimit {
 
     private static MultiPolygon readBerlinShape(GeometryFactory geometryFactory) throws IOException {
-        var file = Paths.get("scenarios", "berlin-v5.5-10pct", "input", "berlin-shp", "berlin.dbf").toFile();
+        var file = Paths.get("scenarios", "berlin-v5.5-10pct", "input", "berlin.dbf").toFile();
         var reader = new ShapefileReader(new ShpFiles(file), true, true, geometryFactory);
         var shape = (MultiPolygon) reader.nextRecord().shape();
         reader.close();
@@ -27,9 +28,19 @@ public class ImplementCityWideSpeedLimit {
         for (var node : network.getNodes().values()) {
             double x = node.getCoord().getX();
             double y = node.getCoord().getY();
-            var isContained = shape.contains(geometryFactory.createPoint(new Coordinate(x, y)));
+            boolean isContained = shape.contains(geometryFactory.createPoint(new Coordinate(x, y)));
             node.getAttributes().putAttribute("berlin", isContained);
         }
+
+        for (var link : network.getLinks().values()){
+            var fromNode = link.getFromNode().getAttributes().getAttribute("berlin");
+            var toNode = link.getToNode().getAttributes().getAttribute("berlin");
+            var type = link.getAttributes().getAttribute("type");
+            if (((boolean) fromNode || (boolean) toNode) && !Objects.equals(type, "motorway")){
+                link.setFreespeed(4.166666666666667);
+            }
+        }
+
         NetworkUtils.writeNetwork(network, Paths.get("scenarios", "berlin-v5.5-10pct", "input", "berlin-v5.5-network-annotated.xml.gz").toString());
     }
 
