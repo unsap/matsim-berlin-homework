f6cbdda9eb19bfcf9971214e8ab78f1d4f93e819
Valentyne Stigloher <stigloher@tu-berlin.de>
2023-05-16 08:54:46 +0200

    Do not raise freespeed when previous freespees lower

--- src/main/java/org/matsim/prepare/ImplementCityWideSpeedLimit.java
+++ src/main/java/org/matsim/prepare/ImplementCityWideSpeedLimit.java
@@ -5,10 +5,14 @@ import org.geotools.data.shapefile.shp.ShapefileReader;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.MultiPolygon;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Node;
 import org.matsim.core.network.NetworkUtils;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class ImplementCityWideSpeedLimit {
@@ -25,19 +29,26 @@ public class ImplementCityWideSpeedLimit {
         GeometryFactory geometryFactory = new GeometryFactory();
         var shape = readBerlinShape(geometryFactory);
         var network = NetworkUtils.readNetwork(Paths.get("scenarios", "berlin-v5.5-10pct", "input", "berlin-v5.5-network.xml.gz").toString());
+        Map<Id<Node>, Boolean> isBerlinByNode = new HashMap<>();
         for (var node : network.getNodes().values()) {
             double x = node.getCoord().getX();
             double y = node.getCoord().getY();
             boolean isContained = shape.contains(geometryFactory.createPoint(new Coordinate(x, y)));
-            node.getAttributes().putAttribute("berlin", isContained);
+            node.getAttributes().putAttribute("isBerlin", isContained);
+            isBerlinByNode.put(node.getId(), isContained);
         }
 
-        for (var link : network.getLinks().values()){
-            var fromNode = link.getFromNode().getAttributes().getAttribute("berlin");
-            var toNode = link.getToNode().getAttributes().getAttribute("berlin");
+        double speedLimit = 15 / 3.6;
+
+        for (var link : network.getLinks().values()) {
+            var fromNode = isBerlinByNode.get(link.getFromNode().getId());
+            var toNode = isBerlinByNode.get(link.getToNode().getId());
             var type = link.getAttributes().getAttribute("type");
-            if (((boolean) fromNode || (boolean) toNode) && !Objects.equals(type, "motorway")){
-                link.setFreespeed(4.166666666666667);
+            if ((fromNode || toNode) && !Objects.equals(type, "motorway") && link.getFreespeed() > speedLimit) {
+                link.setFreespeed(speedLimit);
+                link.getAttributes().putAttribute("isModified", true);
+            } else {
+                link.getAttributes().putAttribute("isModified", false);
             }
         }
 
